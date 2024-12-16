731208ad87b9af0ec42b1cbb2eb79485c237591b
selestino <selestinoolivier@gmail.com>
2024-12-17 01:37:19 +0300

    adding swiper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, earthSharp, earthOutline, addCircleOutline, addCircleSharp } from 'ionicons/icons';
+import { paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, earthSharp, earthOutline, addCircleOutline, addCircleSharp } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,24 +1,43 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { chatbubbles, heart, bookmark } from 'ionicons/icons';
+import { chatbubbles, heart, bookmark, arrowUpCircleOutline, arrowDownCircleOutline } from 'ionicons/icons';
+import { SwiperModule } from 'swiper/angular';
+import SwiperCore, { Navigation } from 'swiper';
+import { CommonModule } from '@angular/common';
 
-addIcons({ heart, chatbubbles, bookmark })
+SwiperCore.use([Navigation]);
+
+addIcons({ heart, chatbubbles, bookmark, arrowUpCircleOutline, arrowDownCircleOutline })
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonIcon],
+  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonIcon, SwiperModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
+  public data: {question: string}[] = [];
+
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.data = [
+      {question: "Tsy omby, tsy ondry nefa mahalala ny maintso hohanina."},
+      {question: "Izy mivady no miady ka ny ankizy no voapotsipotsika."},
+      {question: "Anaty rano tsy lena, an-tanety tsy malazo."},
+      {question: "Elobe tsy mipiaka."},
+      {question: "Alina izy tonga tsy nantsoina, atoandro very tsy nangalarina."},
+      {question: "Akana vao mitombo."},
+      {question: "Tsy basy mipoaka, tsy tafondro mirefotra nefa feno ra Madagasikara."},
+      {question: "Teraka mba lava ihany, antitra vao mitsipozy."},
+    ]
   }
+
 }
